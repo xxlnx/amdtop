53e7c19d523d3ea6d7bbbfbc0ffae26a27b45fa0
Wang Yang <wangyang7902@gmail.com>
2019-07-20 14:50:20 +0800

    tui: add gpu load in statuswindow
    Signed-off-by: Wang Yang <wangyang7902@gmail.com>

--- tui/statuswindow.c
+++ tui/statuswindow.c
@@ -14,14 +14,18 @@ static int update_status(struct Window *win)
     size_t  size = 0;
     char buff[MAX_NAME_SIZE];
     struct GpuMemInfo gpuMemInfo;
+    struct GpuSensorInfo sensorGpuLoadInfo;
     struct Device *dev = getAcitveDevice();
     if (!dev)
         return -EINVAL;
     ret = gpuQueryMemInfo(dev->gpuDevice, MemType_VRAM, &gpuMemInfo);
     if (ret)
         return ret;
+    ret = gpuQuerySensorInfo(dev->gpuDevice, SensorType_GpuLoad, &sensorGpuLoadInfo);
+    if (ret)
+        return ret;
     WindowClear(win);
-    size = snprintf(buff, MAX_NAME_SIZE, "[%ldMB/%ldMB]", gpuMemInfo.used >> 20, ALIGN(gpuMemInfo.total >> 20, 1024));
+    size = snprintf(buff, MAX_NAME_SIZE, "[%ldMB/%ldMB Load:%3d%%]", gpuMemInfo.used >> 20, ALIGN(gpuMemInfo.total >> 20, 1024), sensorGpuLoadInfo.value);
     mvwprintw(win->nwin, 1, 1, "%d:%s [%04x:%02x:%02x.%d]",
               getContext()->activeDeviceID, dev->deviceName,
               dev->domain, dev->bus, dev->dev, dev->func);
