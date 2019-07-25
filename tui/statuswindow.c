5ae1a69e35e8e8f6ba6f4c64c06916c963bc2b95
Wang Yang <wangyang7902@gmail.com>
2019-07-26 00:19:38 +0800

    tui: change status window format
    Signed-off-by: Wang Yang <wangyang7902@gmail.com>

--- tui/statuswindow.c
+++ tui/statuswindow.c
@@ -3,35 +3,51 @@
 #include "context.h"
 #include <errno.h>
 
+#define GPU_LOAD_THRESHOLD  (85)
+
 static struct DeviceContext *dctx = NULL;
 static struct WindowContext *wctx = NULL;
 
-#define ALIGN(val, align)   (((val) + (align - 1)) & ~(align - 1))
-
+#define GPU_LOAD_BAR_WIDHT  (11)    /* strlen [Load:100%] */
 static int update_status(struct Window *win)
 {
     int ret = 0;
     size_t  size = 0;
-    char buff[MAX_NAME_SIZE];
-    struct GpuMemInfo gpuMemInfo;
     struct GpuSensorInfo sensorGpuLoadInfo;
-    struct Device *dev = getAcitveDevice();
-    if (!dev)
-        return -EINVAL;
-    ret = gpuQueryMemInfo(dev->gpuDevice, MemType_VRAM, &gpuMemInfo);
+    struct Device *device = getAcitveDevice();
+    enum ColorType colorType = COLOR_DEAFULT;
+
+    if (!DeviceDriverisLoaded(device))
+        return 0;
+
+    ret = gpuQuerySensorInfo(device->gpuDevice, SensorType_GpuLoad, &sensorGpuLoadInfo);
     if (ret)
         return ret;
-    ret = gpuQuerySensorInfo(dev->gpuDevice, SensorType_GpuLoad, &sensorGpuLoadInfo);
-    if (ret)
-        return ret;
-    WindowClear(win);
-    size = snprintf(buff, MAX_NAME_SIZE, "[%ldMB/%ldMB Load:%3d%%]", gpuMemInfo.used >> 20, ALIGN(gpuMemInfo.total >> 20, 1024), sensorGpuLoadInfo.value);
-    mvwprintw(win->nwin, 1, 1, "%d:%s [%04x:%02x:%02x.%d]",
-              getContext()->activeDeviceID, dev->deviceName,
-              dev->domain, dev->bus, dev->dev, dev->func);
-    mvwprintw(win->nwin, 1, win->layout.width - size - 1, "%s", buff);
-    wrefresh(win->nwin);
+
+    colorType = sensorGpuLoadInfo.value > GPU_LOAD_THRESHOLD ? COLOR_RED_COLOR : COLOR_DEAFULT;
+    mvwprintwc(win->nwin, 1, win->layout.width - GPU_LOAD_BAR_WIDHT - 1, COLOR_DEAFULT, "[Load:");
+    mvwprintwc(win->nwin, 1, getcurx(win->nwin), colorType, "%3d%%", sensorGpuLoadInfo.value);
+    mvwprintwc(win->nwin, 1, getcurx(win->nwin), COLOR_DEAFULT, "]");
+
     return ret;
+}
+
+static int print_device_info(struct Window *win, struct Device *device)
+{
+    WINDOW *nwin = win->nwin;
+
+    mvwprintwc(nwin, 1, 1, COLOR_DEAFULT, "%d: [%04x:%02x:%02x.%d] %s",
+              getContext()->activeDeviceID,
+              device->domain, device->bus, device->dev, device->func,
+              device->deviceName);
+
+    if (DeviceDriverisLoaded(device))
+        mvwprintwc(nwin, 1, getcurx(nwin) + 1, COLOR_DEAFULT, "-- %s",
+            device->driverName);
+    else
+        mvwprintwc(nwin, 1, getcurx(nwin) + 1, COLOR_RED_COLOR, "No Driver");
+
+    return 0;
 }
 
 static int statusInit(struct Window *win)
@@ -39,8 +55,24 @@ static int statusInit(struct Window *win)
     int ret = 0;
     dctx = getContext()->dctx;
     wctx = getContext()->wctx;
-    ret = update_status(win);
-    return ret;
+    struct Device *device = getAcitveDevice();
+
+    if (device) {
+        ret = print_device_info(win, device);
+        if (ret)
+            return 0;
+    } else {
+       /* TODO no device found */
+    }
+
+    if (DeviceDriverisLoaded(device)) {
+        ret = update_status(win);
+        if (ret)
+            return 0;
+    }
+
+    wrefresh(win->nwin);
+    return 0;
 }
 
 static int statusExit(struct Window *win)
@@ -63,7 +95,10 @@ static int statusControl(struct Window *win, int cmd, void *data)
 static int statusUpdate(struct Window *win, uint32_t flags)
 {
     int ret = 0;
+
     ret = update_status(win);
+    wrefresh(win->nwin);
+
     return ret;
 }
 
